76e403d7a75efa889adf02aa90c6591f5eaab27c
raihan <5421449+ra89fi@users.noreply.github.com>
2022-04-04 21:31:54 +0600

    reviews and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import Reviews from './Reviews';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+    const goToReviews = () => {
+        navigate('/reviews');
+    };
     return (
-        <div className="home">
-            <div className="half left">
-                <h1>Shopping Overhaul</h1>
-                <p>
-                    Welcome! This week we are offering excellent T-Shirts at
-                    even more excellent prices. Grab one for you and one for
-                    your loved one too!
-                </p>
-                <p>
-                    <button className="btn btn-primary">Live Demo</button>
-                </p>
+        <div class="content">
+            <div className="home">
+                <div className="half left">
+                    <h1>Shopping Overhaul</h1>
+                    <p>
+                        Welcome! This week we are offering excellent T-Shirts at
+                        even more excellent prices. Grab one for you and one for
+                        your loved one too!
+                    </p>
+                    <p>
+                        <button className="btn btn-primary">Live Demo</button>
+                    </p>
+                </div>
+                <div className="half">
+                    <img
+                        src="https://render.fineartamerica.com/images/rendered/default/t-shirt/23/30/images/artworkimages/medium/1/coffee-coding-syntax-aadil-farooq-transparent.png"
+                        alt="T shirt"
+                    />
+                </div>
             </div>
-            <div className="half">
-                <img
-                    src="https://render.fineartamerica.com/images/rendered/default/t-shirt/23/30/images/artworkimages/medium/1/coffee-coding-syntax-aadil-farooq-transparent.png"
-                    alt="T shirt"
-                />
+            <div className="home-reviews">
+                <h3>Customer Reviews</h3>
+                <Reviews itemNum={3}></Reviews>
+                <p style={{ textAlign: 'center' }}>
+                    <button onClick={goToReviews} className="btn btn-primary">
+                        See All Reviews
+                    </button>
+                </p>
             </div>
         </div>
     );
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import Rating from 'react-rating';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useReviews from '../hooks/useReviews';
+import './Reviews.css';
 
-const Reviews = () => {
-    return <div></div>;
+const Reviews = ({ itemNum }) => {
+    const [reviews, setReviews] = useReviews();
+    const isReviewsPage = itemNum === reviews.length;
+    itemNum = itemNum || reviews.length;
+    reviews.splice(itemNum);
+    return (
+        <div>
+            {!isReviewsPage ? (
+                <h2 style={{ textAlign: 'center', marginTop: '2.5rem' }}>
+                    What our customers say!
+                </h2>
+            ) : (
+                ''
+            )}
+            <div className="reviews">
+                {reviews.map((review) => (
+                    <div className="item">
+                        <div className="head">
+                            <img src="user.jpg" alt="User" />
+                            <h5>{review.name}</h5>
+                        </div>
+                        <p>{review.text}</p>
+                        <Rating
+                            initialRating={review.rating}
+                            emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                            fullSymbol={
+                                <FontAwesomeIcon
+                                    style={{ color: 'goldenrod' }}
+                                    icon={faStar}
+                                />
+                            }
+                            readonly
+                        ></Rating>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Reviews;
